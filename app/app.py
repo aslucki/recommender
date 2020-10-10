6026b3658dc364a07365ffd2a034136e4e5af2a3
aslucki <adam.slucki@gmail.com>
2020-10-10 16:30:02 +0200

    Adding working app

--- app/app.py
+++ app/app.py
@@ -1,10 +1,17 @@
 import json
 import os
 
+import pandas as pd
 from flask import (Flask, render_template,
                    request, make_response, url_for)
 
+from model import Recommender
+
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] =\
+    os.path.join(os.path.dirname(__file__), 'database')
+
+recommender = Recommender(app.config['UPLOAD_FOLDER'])
 
 
 @app.route('/_check')
@@ -14,7 +21,32 @@ def healthcheck():
 
 @app.route('/')
 def home():
-    return render_template('main.html')
+    return render_template('home.html',
+                           companies=recommender.companies)
+
+
+@app.route('/', methods=["POST"])
+def process():
+    company_name = request.form["searchBar"]
+    most_similar_companies = recommender.find_most_similar(company_name)
+    output = table_to_dict(most_similar_companies)
+    return render_template('table.html', query=company_name, table=output,
+                           companies=recommender.companies)
+
+
+@app.route('/companies')
+def show_available_data():
+    output = table_to_dict(recommender.data)
+    return render_template('table.html', table=output,
+                           companies=recommender.companies)
+
+
+def table_to_dict(table: pd.DataFrame):
+
+    columns = table.columns
+    rows = table.values
+    details = "test description of differences and similarities"
+    return {"columns": columns, "rows": rows, "details": details}
 
 
 if __name__ == '__main__':
